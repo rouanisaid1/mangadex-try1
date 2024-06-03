bfb7ef01f79fc761a69688ba779a7e94b73a6049
rouanisaid1 <saidrouani@gmail.com>
2024-06-03 19:53:50 +0100

    Update proxy to remove security headers and update index.html

--- api/proxy.mjs
+++ api/proxy.mjs
@@ -20,18 +20,25 @@ export default async (req, res) => {
     });
 
     console.log('Received response with status:', response.status);
-    
+
     if (!response.ok) {
       console.log('Response not ok');
       res.status(response.status).send('Error fetching the target URL');
       return;
     }
 
+    const headers = {};
+    response.headers.forEach((value, name) => {
+      if (name.toLowerCase() !== 'x-frame-options' && name.toLowerCase() !== 'content-security-policy') {
+        headers[name] = value;
+      }
+    });
+
     const data = await response.text();
     console.log('Response data length:', data.length);
 
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).send(data);
+    res.writeHead(response.status, headers);
+    res.end(data);
   } catch (error) {
     console.error('Error fetching the target URL:', error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
